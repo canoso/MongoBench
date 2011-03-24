68304c33213ebdfe476d34a988b72f181fbed43a
Takaya Okano <o_canon@hotmail.com>
2011-03-24 20:44:33 +0900

    add class.

---
+++ src/util/PropertyReader.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package util;
+
+import java.util.Properties;
+
+/**
+ * @author canoso
+ *
+ */
+public class PropertyReader {
+
+	private static final Properties PROP = new Properties();
+
+	private PropertyReader(){
+		// dummy constructor.
+	}
+
+	public static void readProperty(String fileName){
+
+	}
+}
